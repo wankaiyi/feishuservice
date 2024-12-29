2af6de338f0222317037e4ca1eea3a3ed9080f9a
chenzhihao <111370767+Nacyoooooo@users.noreply.github.com>
2024-12-29 23:30:26 +0800

    pref: 优化功能，将获取chatgpt回复信息的过程异步处理，提高响应速度

--- src/main/java/com/wky/feishuservice/strategy/feishucardbutton/impl/NextQuestionProcessStrategy.java
+++ src/main/java/com/wky/feishuservice/strategy/feishucardbutton/impl/NextQuestionProcessStrategy.java
@@ -12,11 +12,15 @@ import com.wky.feishuservice.service.FeishuMessageService;
 import com.wky.feishuservice.strategy.feishucardbutton.FeishuCardButtonStrategy;
 import com.wky.feishuservice.utils.JacksonUtils;
 import com.wky.feishuservice.utils.UserInfoContext;
+import jakarta.annotation.Resource;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 @Component
 @RequiredArgsConstructor
@@ -24,6 +28,8 @@ public class NextQuestionProcessStrategy implements FeishuCardButtonStrategy {
     private final FeishuMessageService feishuMessageService;
     private final FeishuClient feishuClient;
     private final OpenAiClient openAiClient;
+    @Resource
+    private ThreadPoolTaskExecutor openaiChatThreadPool;
     @Override
     public void handle(String openMessageId, FeishuCallbackResponseDTO response, String token, String question) {
         UserInfo userInfo = UserInfoContext.getUserInfo();
@@ -35,10 +41,24 @@ public class NextQuestionProcessStrategy implements FeishuCardButtonStrategy {
         FeishuClient self = SpringUtil.getBean(FeishuClient.class);
         String format = String.format(defalutCard, JacksonUtils.serialize(map));
         self.sendFeishuP2pMsg(format,receiveId,"open_id","interactive",null);
-        ChatResponseBO chatResponseBO = openAiClient.chat(receiveId, question);
-        feishuClient.sendP2pMsg(chatResponseBO, receiveId, receiveType, "post", openMessageId);
-        ChatResponseBO predictNextQuestion =openAiClient.getPredictNextQuestion(receiveId,question);
-        feishuClient.sendP2PPredictQuestion(predictNextQuestion, receiveId, receiveType, "interactive", openMessageId);
+        CompletableFuture<ChatResponseBO> aiTask = CompletableFuture.supplyAsync(() -> {
+            ChatResponseBO chatResponseBO = openAiClient.chat(receiveId, question);
+            return chatResponseBO;
+        }, openaiChatThreadPool);
+        CompletableFuture<ChatResponseBO> questionTask = CompletableFuture.supplyAsync(() -> {
+            ChatResponseBO predictNextQuestion =openAiClient.getPredictNextQuestion(receiveId,question);
+            return predictNextQuestion;
+        }, openaiChatThreadPool);
+        CompletableFuture.allOf(aiTask,questionTask).thenRun(()->{
+            try {
+                ChatResponseBO chatResponseBO = aiTask.get();
+                feishuClient.sendP2pMsg(chatResponseBO, receiveId, receiveType, "post", openMessageId);
+                ChatResponseBO predictNextQuestion = questionTask.get();
+                feishuClient.sendP2PPredictQuestion(predictNextQuestion, receiveId, receiveType, "interactive", openMessageId);
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+        }).join();
     }
     private String defalutCard= """
             {
